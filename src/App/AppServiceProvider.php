ebbb2474085612caad983280e05329986180689a
vgarciaf <vgarciaf@users.noreply.github.com>
2022-12-20 11:25:07 +0000

    Commit from GitHub Actions (style-fix)

--- src/App/AppServiceProvider.php
+++ src/App/AppServiceProvider.php
@@ -2,11 +2,11 @@
 
 namespace Omnipay\OfflineDummy\App;
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Omnipay\OfflineDummy\App\Http\Controllers\PaymentController;
 use Omnipay\OfflineDummy\App\Http\Controllers\PaymentProcessController;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 class AppServiceProvider extends ServiceProvider
 {
